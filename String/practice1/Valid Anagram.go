1310e3786da8211359ed9c74d91e6fe48160d89e
Allen <spy1k387j6@gmail.com>
2025-03-05 10:47:35 +0800

    validAnagram

--- String/practice1/Valid Anagram.go
+++ String/practice1/Valid Anagram.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -11,19 +10,47 @@ func main() {
 }
 
 func isAnagram(s string, t string) bool {
+	//slice string and compare diffrent
+	// if len(s) != len(t) {
+	// 	return false
+	// }
+
+	// sArr := []rune(s)
+	// tArr := []rune(t)
+
+	// sort.Slice(sArr, func(i int, j int) bool { return sArr[i] < sArr[j] })
+	// sort.Slice(tArr, func(i int, j int) bool { return tArr[i] < tArr[j] })
+
+	// if string(sArr) == string(tArr) {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
+
+	//set
+	// 如果長度不相同，直接返回 false
 	if len(s) != len(t) {
 		return false
 	}
 
-	sArr := []rune(s)
-	tArr := []rune(t)
+	// 用 map 來統計每個字母的出現次數
+	counts := make(map[rune]int)
 
-	sort.Slice(sArr, func(i int, j int) bool { return sArr[i] < sArr[j] })
-	sort.Slice(tArr, func(i int, j int) bool { return tArr[i] < tArr[j] })
-
-	if string(sArr) == string(tArr) {
-		return true
-	} else {
-		return false
+	// 遍歷第一個字串，統計每個字符的數量
+	for _, char := range s {
+		counts[char]++
 	}
+
+	// 遍歷第二個字串，減少每個字符的數量
+	for _, char := range t {
+		counts[char]--
+		// 如果某個字符的數量小於 0，則不是字謎
+		if counts[char] < 0 {
+			return false
+		}
+	}
+
+	// 如果所有字符的數量都為 0，則是字謎
+	return true
+
 }
